2371b06d5a84f6a8f8552423eeb5a61abb2df26c
krasniy23 <krasnoschek23@gmail.com>
2022-09-27 15:19:03 +0300

    HW4

--- src/main/java/org/example/HW4.java
+++ src/main/java/org/example/HW4.java
@@ -1,7 +1,13 @@
 package org.example;
 
 public class HW4 {
-    chat[] chars = [5,6,7,12,-5,32,43];
-    String s = new String(shars);
-    System.out.println(s);
+    public static void main(String[] args) {
+        int [] arr = new int [] {5,6,7,12,-5,32,43};
+        int sum = 0;
+        for (int j : arr) {
+            sum = sum + j;
+        }
+        System.out.println("Sum of all the elements of an array: " + sum);
+    }
 }
+
