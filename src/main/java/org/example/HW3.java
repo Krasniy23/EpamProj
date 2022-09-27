7d3277561bf6cb27f0ed7caaa8239a2ed3cf06f4
krasniy23 <krasnoschek23@gmail.com>
2022-09-27 12:46:03 +0300

    HW3

--- src/main/java/org/example/HW3.java
+++ src/main/java/org/example/HW3.java
@@ -1,5 +1,31 @@
 package org.example;
 
-public class HW3 {
+import java.sql.SQLOutput;
+import java.util.Scanner;
 
+public class HW3 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Input first number: ");
+        String input1 = sc.nextLine();
+        double d1 = Double.parseDouble(input1);
+
+        System.out.print("Input second number: ");
+        String input2 = sc.nextLine();
+        double d2 = Double.parseDouble(input2);
+
+        double result = d1 + d2;
+        double result1 = d1 - d2;
+        double result2 = d1 * d2;
+        double result3 = d1 / d2;
+
+
+        System.out.println("Answer by sum " + result);
+        System.out.println("Answer by subtract " + result1);
+        System.out.println("Answer by multiply " + result2);
+        System.out.println("Answer by divide  " + result3);
+
+    }
 }
