6c56b9b76438ac7c827c72e55fb3c186d223144d
krasniy23 <krasnoschek23@gmail.com>
2022-10-05 22:09:48 +0300

    P2HW2

---
+++ src/main/java/org/example/P2HW3.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.awt.*;
+
+public class P2HW3 {
+    public static void main(String[] args) {
+        Rectangle myRect = null;
+        myRect.width = 40;
+        myRect.height = 50;
+        System.out.println("myRect's area is " + myRect.area());
+    }
+}
