50f9a8ae45cf0392a012164ae5471716619bd7eb
krasniy23 <krasnoschek23@gmail.com>
2022-10-05 20:42:31 +0300

    random

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,11 @@
-package org.example;
+import java.util.Random;
 
 public class Main {
-    private static String[] months =
-            {"January","february","March", "April","January1","february1","March1", "April1"};
     public static void main(String[] args) {
+        Random random = new Random();
+        System.out.println(random.nextInt());
 
-        loopMonths();
-    }
-
-    private static void loopMonths() {
-        for (int i = 0; i < months.length; i++) {
-            System.out.println(months[i]);
-        }
+        System.out.println(random.nextletter());
+        
     }
 }
