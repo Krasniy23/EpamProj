9217122d0851de4f2352a3f6006cc649aa2b7035
krasniy23 <krasnoschek23@gmail.com>
2022-10-05 21:35:20 +0300

    P2HW1

---
+++ src/main/java/org/example/P2HW1.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class P2HW1 {
+    public static void main(String[] args) {
+        String str1 = "PHP Exercises and ";
+        String str2 = "Python Exercises";
+        System.out.println(("The concatenated string with replacements:" + str1 + str2).replace("P", ""));
+    }
+}
