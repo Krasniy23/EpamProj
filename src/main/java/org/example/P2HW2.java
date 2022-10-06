d44a9a8db54beefea51878a7871c6bb5e9a552ff
krasniy23 <krasnoschek23@gmail.com>
2022-10-06 17:39:01 +0300

    P2HW2

--- src/main/java/org/example/P2HW2.java
+++ src/main/java/org/example/P2HW2.java
@@ -3,8 +3,6 @@ package org.example;
 public class P2HW2 {
     private String brand;
     private String screenSize;
-    private String numberOfCores;
-    private String type;
     public P2HW2 (String brand, String screenSize){
         this.brand = brand;
         this.screenSize = screenSize;
