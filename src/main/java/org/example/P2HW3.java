782f300d9f6ef59e608f3f4b3270f8412f501f02
krasniy23 <krasnoschek23@gmail.com>
2022-10-06 16:59:19 +0300

    P2HW3

--- src/main/java/org/example/P2HW3.java
+++ src/main/java/org/example/P2HW3.java
@@ -1,11 +1,12 @@
 package org.example;
 import java.awt.*;
-
 public class P2HW3 {
     public static void main(String[] args) {
-        Rectangle myRect = null;
-        myRect.width = 40;
-        myRect.height = 50;
-        System.out.println("myRect's area is " + myRect.area());
+        Rectangle myRect = new Rectangle();
+        int x = 40;
+        int y = 50;
+        myRect.width = x;
+        myRect.height = y;
+        System.out.println("myRect's area is " + myRect);
     }
 }
