14f76b0708c646bd4f3b497f473cb745734201e8
krasniy23 <krasnoschek23@gmail.com>
2022-10-06 17:20:05 +0300

    P2HW1

--- src/main/java/org/example/P2HW1.java
+++ src/main/java/org/example/P2HW1.java
@@ -4,6 +4,6 @@ public class P2HW1 {
     public static void main(String[] args) {
         String str1 = "PHP Exercises and ";
         String str2 = "Python Exercises";
-        System.out.println(("The concatenated string with replacements:" + str1 + str2).replace("P", ""));
+        System.out.println(("The concatenated string with replacements:" + str1 + str2).replace("P", "").replace("p",""));
     }
 }
