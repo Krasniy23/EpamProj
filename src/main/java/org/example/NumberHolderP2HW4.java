c6605c949d6a660c5d6851a60159d35e8bc68029
krasniy23 <krasnoschek23@gmail.com>
2022-10-19 13:37:49 +0300

    P2HW4

---
+++ src/main/java/org/example/NumberHolderP2HW4.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class NumberHolderP2HW4 {
+    public int anInt;
+    public float aFloat;
+
+    public int getAnInt() {
+        return anInt;
+    }
+
+    public void setAnInt(int anInt) {
+        this.anInt = anInt;
+    }
+
+    public float getaFloat() {
+        return aFloat;
+    }
+
+    public void setaFloat(float aFloat) {
+        this.aFloat = aFloat;
+    }
+}
---
+++ src/main/java/org/example/NumberHolderP2HW4Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class NumberHolderP2HW4Main {
+    public static void main(String[] args) {
+        NumberHolderP2HW4 numberObject = new NumberHolderP2HW4();
+        numberObject.anInt = 12;
+        numberObject.aFloat = 13;
+        System.out.println(numberObject.aFloat + " " + numberObject.anInt);
+    }
+}
