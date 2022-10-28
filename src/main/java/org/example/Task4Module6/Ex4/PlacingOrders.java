cd47e600a585eecac49b021a80965bd9d9d39ed4
OnipkoAlex <onipko94.o@gmail.com>
2022-10-28 21:18:25 +0300

    Changes

--- src/main/java/org/example/Task4Module6/Ex4/PlacingOrders.java
+++ src/main/java/org/example/Task4Module6/Ex4/PlacingOrders.java
@@ -1,28 +1,38 @@
 package org.example.Task4Module6.Ex4;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.TreeSet;
 
 public class PlacingOrders {
+
     public static void main(String[] args) {
+        String[] orders = {"AT&T", "Verizon", "Orange", "GTS", "Talk Talk", "GTS"};
+
         HashSet<String> hSet = new HashSet<>();
 
-        hSet.add("AT&T");
-        hSet.add("Verizon");
-        hSet.add("Orange");
-        hSet.add("GTS");
-        hSet.add("Talk Talk");
-        hSet.add(null);
-        hSet.add(null);
+//        hSet.add("AT&T");
+//        hSet.add("Verizon");
+//        hSet.add("Orange");
+//        hSet.add("GTS");
+//        hSet.add("Talk Talk");
+//        hSet.add(null);
+//        hSet.add(null);
+
+        hSet.addAll(Arrays.asList(orders));
         System.out.println(hSet);
 
         TreeSet<String> tSets = new TreeSet<>();
 
-        tSets.add("AT&T");
-        tSets.add("Verizon");
-        tSets.add("Orange");
-        tSets.add("GTS");
-        tSets.add("Talk Talk");
+//        tSets.add("AT&T");
+//        tSets.add("Verizon");
+//        tSets.add("Orange");
+//        tSets.add("GTS");
+//        tSets.add("Talk Talk");
+
+        for (String order : orders) {
+            tSets.add(order);
+        }
         System.out.println(tSets);
     }
 }
