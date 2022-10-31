9c38baa54e8e72589210d128ac3328798846b670
OnipkoAlex <onipko94.o@gmail.com>
2022-10-31 17:35:35 +0200

    combine method implemented

--- src/main/java/org/example/Task4Module6/Ex5/TwoMap.java
+++ src/main/java/org/example/Task4Module6/Ex5/TwoMap.java
@@ -12,17 +12,25 @@ import java.util.stream.Stream;
 public class TwoMap {
 
     public static void main(String[] args) {
-        ArrayList<String> firstTeam = new ArrayList<>(Arrays.asList("Charles", "Evelyn", "Madison", "Lily", "Dora", "Michelle"));
+        ArrayList<String> firstTeam = new ArrayList<>(Arrays.asList("Charles", "Evelyn", "Madison", "Lily", "Dora", "Michelle", "Cindy", "Vera"));
         ArrayList<String> secondTeam = new ArrayList<>(Arrays.asList("Mila", "Isabella", "Lucy", "Logan"));
-
-//        List<String> combineList = Stream.of(firstTeam, secondTeam).flatMap(Collection::stream).sorted().collect(Collectors.toList());
-//        System.out.println(combineList);
 
         System.out.println(combine(firstTeam, secondTeam));
     }
 
     public static <K, V> Map<K, V> combine(List<K> listOne, List<V> listTwo) {
         Map<K, V> resultMap = new HashMap<>();
+
+        for (int i = 0; i < listTwo.size(); i++) {
+            if (listTwo.get(i) == listTwo.get(listTwo.size() - 1)) {
+                resultMap.put(listOne.get(i), listTwo.get(i));
+                for (int j = i + 1; j < listOne.size(); j++) {
+                    resultMap.put(listOne.get(j), null);
+                }
+            } else {
+                resultMap.put(listOne.get(i), listTwo.get(i));
+            }
+        }
 
         return resultMap;
     }
