16224c1e85e52c04582c3d4965b720f7fdc33226
krasniy23 <krasnoschek23@gmail.com>
2022-11-17 15:06:32 +0200

    Task51and2

---
+++ src/main/java/org/example/Task5/Library.java
@@ -0,0 +1,14 @@
+package org.example.Task5;
+
+public class Library {
+    static class Books {
+        public final String book;
+        public final Integer price;
+
+        public Books(String book, Integer price) {
+            this.book = book;
+            this.price = price;
+        }
+    }
+
+}
---
+++ src/main/java/org/example/Task5/UniPerson.java
@@ -0,0 +1,43 @@
+package org.example.Task5;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+public class UniPerson {
+    static class Person {
+        public final String firstName;
+        public final String lastName;
+        public final Integer age;
+
+        public Person(String firstName, String lastName, Integer age) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+        }
+
+        public static void main(String[] args) {
+            Person[] peopleArr = {
+                    new Person("Peter", "Patterson", 21),
+                    new Person("Paul", "Walker", 31),
+                    new Person("Steve", "Runner", 17),
+                    new Person("Arnold", "", -1),
+                    new Person(" ", "Stevenson", 19),
+                    new Person("   ", "Stevenson", 19),
+                    new Person("      ", "Stevenson", 19),
+                    new Person(" Arnold", "Stevenson", 19),
+                    new Person("Aaron", "Bortnicker", 18),
+            };
+            List <Person> people = new ArrayList<>(Arrays.asList(peopleArr));
+
+            List<String> peopleName = people
+                    .stream()
+                    .map((person) -> peopleName)
+                    .collect(Collectors.toList());
+
+            System.out.println(peopleName);
+        }
+    }
+}
