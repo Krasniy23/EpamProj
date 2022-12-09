c91f5e76022154ac6813a4be1c21cc3de6bae53c
krasniy23 <krasnoschek23@gmail.com>
2022-12-09 19:31:48 +0200

    Fix css selectors path

--- src/main/java/org/example/Task6/BookDepository.java
+++ src/main/java/org/example/Task6/BookDepository.java
@@ -11,9 +11,9 @@ public class BookDepository {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.bookdepository.com/");
         Thread.sleep(2000);
-        WebElement cookiesYes = driver.findElement(By.cssSelector(".cookie-consent-buttons [class*='yes']"));
-        cookiesYes.click();
-        Thread.sleep(2000);
+//        WebElement cookiesYes = driver.findElement(By.cssSelector(".cookie-consent-buttons [class*='yes']"));
+//        cookiesYes.click();
+//        Thread.sleep(2000);
         By searchGradleButton = By.xpath("//*[@id=\"book-search-form\"]/div[1]/input[1]");
         WebElement searchField = driver.findElement(searchGradleButton);
         searchField.sendKeys("Harry Potter");
@@ -21,19 +21,19 @@ public class BookDepository {
         searchButton.click();
         Thread.sleep(2000);
         WebElement selectBook = driver.findElement(By.cssSelector(
-                "[class='tab search'] .title"));
+                "[class='book-item'] .title"));
         selectBook.click();
         Thread.sleep(2000);
         WebElement addToBasket = driver.findElement(By.cssSelector(
-                "body > div.page-slide > div.content-wrap > div > div > div.item-wrap > div.item-block > div.item-tools > div > div.btn-wrap > a.btn.btn-primary.add-to-basket"));
+                "[class='btn btn-primary add-to-basket']"));
         addToBasket.click();
         Thread.sleep(2000);
         WebElement goToBasket = driver.findElement(By.cssSelector(
-                "body > div.modal.fade.status-success.in > div > div > div.modal-body > div > div.basket-info > a.btn.btn-primary.pull-right.continue-to-basket.string-to-localize.link-to-localize"));
+                "[class='btn btn-primary pull-right continue-to-basket string-to-localize link-to-localize']"));
         goToBasket.click();
         Thread.sleep(2000);
         WebElement removeFromBasket = driver.findElement(By.cssSelector(
-                "body > div.page-slide > div.content-wrap > div > div.basket-items-wrap > div.basket-item > div.item-checkout-info > form.remove-item > button"));
+                "[class='btn remove-btn']"));
         removeFromBasket.click();
         Thread.sleep(20000);
         driver.quit();
