7ee40670bf56c858a5c7695aabddb8526edf4a23
krasniy23 <krasnoschek23@gmail.com>
2022-12-26 19:28:41 +0200

    JUnit tests

--- src/main/test/BookTests.java
+++ src/main/test/BookTests.java
@@ -17,6 +17,24 @@ public class BookTests {
     @Test
     public void checkThatUrlIsValid() {
         Assert.assertEquals("https://www.bookdepository.com/", driver.getCurrentUrl());
+        By searchGradleButton = By.xpath("//*[@id=\"book-search-form\"]/div[1]/input[1]");
+        WebElement searchField = driver.findElement(searchGradleButton);
+        searchField.sendKeys("Harry Potter");
+        WebElement searchButton = driver.findElement(By.xpath("//*[@id=\"book-search-form\"]/div[1]/button"));
+        searchButton.click();
+        Thread.sleep(2000);
+        WebElement selectBook = driver.findElement(By.cssSelector(
+                "[class='book-item'] .title"));
+        selectBook.click();
+        Thread.sleep(2000);
+        WebElement addToBasket = driver.findElement(By.cssSelector(
+                "[class='btn btn-primary add-to-basket']"));
+        addToBasket.click();
+        Thread.sleep(2000);
+        WebElement goToBasket = driver.findElement(By.cssSelector(
+                "[class='btn btn-primary pull-right continue-to-basket string-to-localize link-to-localize']"));
+        goToBasket.click();
+        
     }
 
     @After
